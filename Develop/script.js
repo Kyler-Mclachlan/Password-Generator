482ccd3c077275b1b8d52f011bbf3bead7b9aa66
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2021-12-10 18:39:13 -0600

    added values to password

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,20 @@
 // Assignment code here
-
-var input = [];
+var input = '';
 var inputLength = 0;
 var maxLength = 128;
+var allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-var generatePassword =function(input) {
+var generatePassword =function() {
   /* while for putting limits on min and max */
   while (inputLength < 8 || (inputLength != null && inputLength > maxLength)) {
-    inputLength = window.prompt('Please enter some data. It should be no more than ' + maxLength + ' characters in length');
+    inputLength = window.prompt('Please confirm how long you want your password to be. It should be no more than ' + maxLength + ' characters in length');
   }
+  passwordLength = inputLength;
+  for (var i = 0; i < passwordLength; i++)
+    input += allCharacters.charAt(Math.floor(Math.random() * passwordLength));
+  console.log(passwordLength)
   console.log(inputLength)
+  return input;
   /* initial prompt*/
  var initalPrompt = window.prompt("Hello! Would you like your password to contain ?");
   /*yes or no response*/
@@ -18,7 +23,7 @@ var generatePassword =function(input) {
     var confirmSkip = window.confirm("Are you sure you'd like to quit?")
   }
 }
-
+console.log(input)
 function onclick(){
 
 }
