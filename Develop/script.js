681c4cd2038d46482a93d4d6519693d4ee053b9d
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2021-12-10 19:56:34 -0600

    added lowercase and numbers

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,6 @@ var specialCharacters = "";
 var uppercase = "";
 var lowercase = "";
 var numbers = "";
-var allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()?><{}/*-+";
 /* var allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()?><{}/*-+"; */
 
 var generatePassword =function() {
@@ -19,26 +18,38 @@ var generatePassword =function() {
   if (initalPrompt === "NO" || initalPrompt === "No" || initalPrompt === "no") {
       var specialCharacters = "";
     }
-  /* Uppercase */
+  
+    /* Uppercase */
   var secondPrompt = window.prompt("Would you like your password to contain Uppercase characters?") 
-  if (secondPrompt === "YES" || initalPrompt === "Yes" || initalPrompt === "yes"){
+  if (secondPrompt === "YES" || secondPrompt === "Yes" || secondPrompt === "yes"){
     var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     }
   if (secondPrompt === "NO" || secondPrompt === "No" || secondPrompt === "no"){
     var upperCase = "";
     }
+  
+    /*Special characters and no uppercase*/
   var thirdPrompt = window.prompt("Would you like your password to contain Lowercase characters?") 
-  if (thirdPrompt === "YES" || thirdPrompt === "Yes" || thirdPrompt === "yes"){
-    var lowercase = "abcdefghijklmnopqrstuvwxyz";
-    }
-    /*special characters and no uppercase*/
-  if (thirdPrompt === "NO" || thirdPrompt === "No" || secondPrompt === "no"){
-    var lowercase = "";
-    }
-    while (inputLength < 8 || (inputLength != null && inputLength > maxLength)) {
+    if (thirdPrompt === "YES" || thirdPrompt === "Yes" || thirdPrompt === "yes"){
+      var lowercase = "abcdefghijklmnopqrstuvwxyz";
+      }
+    if (thirdPrompt === "NO" || thirdPrompt === "No" || thirdPrompt === "no"){
+      var lowercase = "";
+      }
+
+  var forthPrompt = window.prompt("Would you like your password to contain numbers ?") 
+    if (forthPrompt === "YES" || forthPrompt === "Yes" || forthPrompt === "yes"){
+      var numbers = "0123456789";
+      }
+    if (forthPrompt === "NO" || forthPrompt === "No" || forthPrompt === "no"){
+      var numbers = "";
+      }   
+
+     /*setting length requirements */ 
+  while (inputLength < 8 || (inputLength != null && inputLength > maxLength)) {
     inputLength = window.prompt('Please confirm how long you want your password to be. It should be no more than ' + maxLength + ' characters in length');
     }
-    selectedCharacters = specialCharacters + upperCase + lowercase
+    selectedCharacters = specialCharacters + upperCase + lowercase + numbers
     console.log(selectedCharacters)
     console.log(lowercase)
     console.log(upperCase)
