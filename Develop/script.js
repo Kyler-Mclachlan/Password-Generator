e866ae2fe6c3eb2ed14da668256b2fb40a746c6c
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2021-12-10 18:18:41 -0600

    added length contraints

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,27 @@
 // Assignment code here
 
+var input = [];
+var inputLength = 0;
+var maxLength = 128;
+
+var generatePassword =function(input) {
+  /* while for putting limits on min and max */
+  while (inputLength < 8 || (inputLength != null && inputLength > maxLength)) {
+    inputLength = window.prompt('Please enter some data. It should be no more than ' + maxLength + ' characters in length');
+  }
+  console.log(inputLength)
+  /* initial prompt*/
+ var initalPrompt = window.prompt("Hello! Would you like your password to contain ?");
+  /*yes or no response*/
+  if (initalPrompt === "YES" || initalPrompt === "YES") {
+    // confirm user wants to make a password
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?")
+  }
+}
+
+function onclick(){
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
