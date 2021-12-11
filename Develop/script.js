66d658eef74c9fa51b5bf21271f9abbc4752aa88
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2021-12-10 19:46:23 -0600

    added lowercase

--- Develop/script.js
+++ Develop/script.js
@@ -2,22 +2,69 @@
 var input = '';
 var inputLength = 0;
 var maxLength = 128;
+var specialCharacters = "";
+var uppercase = "";
+var lowercase = "";
+var numbers = "";
+var allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()?><{}/*-+";
 /* var allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()?><{}/*-+"; */
 
 var generatePassword =function() {
-  var allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()?><{}/*-+";
 
-  /* asking if the user wants special characters */
+  /*Asking if they want special characters*/
+  var initalPrompt = window.prompt("Would you like your password to contain Special characters?") 
+  if (initalPrompt === "YES" || initalPrompt === "Yes" || initalPrompt === "yes") {
+    var specialCharacters = "!@#$%^&*()?><{}/*-+";
+    }
+  if (initalPrompt === "NO" || initalPrompt === "No" || initalPrompt === "no") {
+      var specialCharacters = "";
+    }
+  /* Uppercase */
+  var secondPrompt = window.prompt("Would you like your password to contain Uppercase characters?") 
+  if (secondPrompt === "YES" || initalPrompt === "Yes" || initalPrompt === "yes"){
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+  if (secondPrompt === "NO" || secondPrompt === "No" || secondPrompt === "no"){
+    var upperCase = "";
+    }
+  var thirdPrompt = window.prompt("Would you like your password to contain Lowercase characters?") 
+  if (thirdPrompt === "YES" || thirdPrompt === "Yes" || thirdPrompt === "yes"){
+    var lowercase = "abcdefghijklmnopqrstuvwxyz";
+    }
+    /*special characters and no uppercase*/
+  if (thirdPrompt === "NO" || thirdPrompt === "No" || secondPrompt === "no"){
+    var lowercase = "";
+    }
+    while (inputLength < 8 || (inputLength != null && inputLength > maxLength)) {
+    inputLength = window.prompt('Please confirm how long you want your password to be. It should be no more than ' + maxLength + ' characters in length');
+    }
+    selectedCharacters = specialCharacters + upperCase + lowercase
+    console.log(selectedCharacters)
+    console.log(lowercase)
+    console.log(upperCase)
+    console.log(specialCharacters)
+    console.log(inputLength)
+    while (input.length < inputLength){
+    for (var i = 0; i < inputLength; i++)
+      input += selectedCharacters.charAt(Math.floor(Math.random() * inputLength))
+      console.log(input);
+    }
+  return input   
+  }
+  
+  /*
+
   var initalPrompt = window.prompt("Hello! Would you like your password to contain Special Characters like @'s and !;s ?");
-  /*yes or no response*/
+ 
   if (initalPrompt === "YES" || initalPrompt === "Yes" || initalPrompt === "yes") {
     var allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()?><{}/*-+";
   }
   if (initalPrompt === "NO" || initalPrompt === "No" || initalPrompt === "no") {
     var allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   }
+
   
-  /* while for putting limits on min and max */
+
   
   while (inputLength < 8 || (inputLength != null && inputLength > maxLength)) {
     inputLength = window.prompt('Please confirm how long you want your password to be. It should be no more than ' + maxLength + ' characters in length');
@@ -31,9 +78,9 @@ var generatePassword =function() {
   return input
 }
 console.log(input)
-function onclick(){
+function onclick(){ */
 
-}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
