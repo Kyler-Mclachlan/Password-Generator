34f2d49b73453a2b7f395180520cf4ec22f21a83
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2021-12-10 19:07:46 -0600

    added speical character question

--- Develop/script.js
+++ Develop/script.js
@@ -2,10 +2,23 @@
 var input = '';
 var inputLength = 0;
 var maxLength = 128;
-var allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+/* var allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()?><{}/*-+"; */
 
 var generatePassword =function() {
+  var allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()?><{}/*-+";
+
+  /* asking if the user wants special characters */
+  var initalPrompt = window.prompt("Hello! Would you like your password to contain Special Characters like @'s and !;s ?");
+  /*yes or no response*/
+  if (initalPrompt === "YES" || initalPrompt === "Yes" || initalPrompt === "yes") {
+    var allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()?><{}/*-+";
+  }
+  if (initalPrompt === "NO" || initalPrompt === "No" || initalPrompt === "no") {
+    var allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  }
+  
   /* while for putting limits on min and max */
+  
   while (inputLength < 8 || (inputLength != null && inputLength > maxLength)) {
     inputLength = window.prompt('Please confirm how long you want your password to be. It should be no more than ' + maxLength + ' characters in length');
   }
@@ -14,14 +27,8 @@ var generatePassword =function() {
     input += allCharacters.charAt(Math.floor(Math.random() * passwordLength));
   console.log(passwordLength)
   console.log(inputLength)
-  return input;
-  /* initial prompt*/
- var initalPrompt = window.prompt("Hello! Would you like your password to contain ?");
-  /*yes or no response*/
-  if (initalPrompt === "YES" || initalPrompt === "YES") {
-    // confirm user wants to make a password
-    var confirmSkip = window.confirm("Are you sure you'd like to quit?")
-  }
+  
+  return input
 }
 console.log(input)
 function onclick(){
